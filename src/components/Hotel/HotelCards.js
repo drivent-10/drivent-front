a8e3fa60931a6c15d54706809c146f526041fa29
hardkill551 <hardkill194@gmail.com>
2023-08-04 14:48:05 -0300

    feat/hotelErrorFinished

--- src/components/Hotel/HotelCards.js
+++ src/components/Hotel/HotelCards.js
@@ -5,25 +5,38 @@ import useHotel from '../../hooks/api/useHotel';
 import { useEffect } from 'react';
 import { useState } from 'react';
 import useRoom from '../../hooks/api/useRoom';
+import Notice from '../Notice';
 
 export default function HotelCards({ hotelId, setHotelId }) {
-  const { hotel } = useHotel();
+  const { hotel, hotelError } = useHotel();
   const [hotels, setHotels] = useState();
-
+  const [message, setMessage] = useState();
   useEffect(() => {
     if (hotel) {
       setHotels(hotel);
     }
-  }, [hotel]);
+    if (hotelError) {
+      if (hotelError.response.data.type === 'unpaid') {
+        setMessage('Você precisa ter confirmado pagamento antes de fazer a escolha de hospedagem');
+      }
+      else if (hotelError.response.data.type === 'unavailable') {
+        setMessage('Sua modalidade de ingresso não inclui hospedagem, prossiga para a escolha de atividades');
+      }
+    }
+  }, [hotel, hotelError]);
 
   return (
     <>
       <Subtitle>Primeiro, escolha seu hotel</Subtitle>
-      <Hotels>
-        {hotels?.map((h) => (
-          <HotelCard key={h.id} {...h} hotelId={hotelId} setHotelId={setHotelId} />
-        ))}
-      </Hotels>
+      {hotelError ? (
+        <Notice>{message}</Notice>
+      ) : (
+        <Hotels>
+          {hotels?.map((h) => (
+            <HotelCard key={h.id} {...h} hotelId={hotelId} setHotelId={setHotelId} />
+          ))}
+        </Hotels>
+      )}
     </>
   );
 }
